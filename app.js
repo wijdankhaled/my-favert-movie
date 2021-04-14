6e6e528e8d316b43d89e954afb054a174c6cf018
wijdankhaled98 <>
2021-04-14 12:04:59 +0000

    shorlec holmes movie

---
+++ app.js
@@ -0,0 +1,9 @@
+var value= prompt ('do you know dr watson pleas answer yes or no');
+if (value =="yes"){
+alert ('that is great enjoy the movie');
+}else if(value == "no"){
+  document.write('<img src="https://static.wikia.nocookie.net/heroes-and-villain/images/5/55/Profile_-_John_Watson_%28BBC%29.jpg/revision/latest/scale-to-width-down/333?cb=20190720063701">');
+  
+  
+}
+
